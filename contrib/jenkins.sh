e2590c0d4c0a664946fb25766f77a2897805026e
Oliver Smith <osmith@sysmocom.de>
2022-02-03 14:01:04 +0100

    contrib/jenkins.sh: new file
    Build the kernel module against a given linux tree. This script will be
    used in CI at jenkins.osmocom.org.
    
    Related: OS#5407
    Depends: docker-playground Id72d19ad08681cd7cb3194de2226292f19e96df5
    Change-Id: I904ab66a1ecd72492642ac2cc4cb102c7283c590

---
+++ contrib/jenkins.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -ex
+# Build the kernel module against the linux tree in KSRC
+
+DEFCONFIG="${DEFCONFIG:-x86_64_defconfig}"
+TOPDIR="$(realpath "$(dirname "$(realpath "$0")")/..")"
+KSRC="${KSRC:-$TOPDIR/../linux}"
+JOBS="${JOBS:-9}"
+
+if ! [ -d "$KSRC" ]; then
+	set +x
+	echo "ERROR: KSRC does not exist: $KSRC"
+	echo "Let the KSRC env var point at a linux source tree and try again."
+	exit 1
+fi
+
+cd "$KSRC"
+
+if ! [ -e ".config" ]; then
+	make "$DEFCONFIG"
+fi
+
+if [ "arch/x86/configs/$DEFCONFIG" -nt ".config" ]; then
+	set +x
+	echo "ERROR: .config inside kernel source tree is older than $DEFCONFIG"
+	echo "Move/delete/touch .config and try again."
+	exit 1
+fi
+
+git log -1 --pretty="%t - %s"
+
+make -j "$JOBS" modules_prepare
+make -j "$JOBS" -C "$TOPDIR" KSRC="$KSRC"
